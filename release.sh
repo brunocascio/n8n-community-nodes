740407323e77b68fb5b77b9dd5471f2d7a82d1ee
Bruno Cascio <brunocascio@gmail.com>
2025-02-17 16:07:57 -0300

    fix release

--- release.sh
+++ release.sh
@@ -5,6 +5,6 @@ VERSION=$(cat Dockerfile | grep FROM | awk -F':' '{print $2}')
 
 IMAGE=br1cascio/n8n:$VERSION
 
-docker build --progress=plain -t $IMAGE .
+docker build --platform linux/amd64 --progress=plain -t $IMAGE .
 
 docker push $IMAGE
