b87db58d79707de452a1991c3b4f9d1dd5dbc2e1
Bruno Cascio <brunocascio@gmail.com>
2025-02-26 15:42:22 -0300

    push latest

--- release.sh
+++ release.sh
@@ -4,7 +4,9 @@ set -e
 VERSION=$(cat Dockerfile | grep FROM | awk -F':' '{print $2}')
 
 IMAGE=br1cascio/n8n:$VERSION
+IMAGE_LATEST=br1cascio/n8n:latest
 
-docker build --platform linux/amd64 --progress=plain -t $IMAGE .
+docker build --platform linux/amd64 --progress=plain -t $IMAGE -t $IMAGE_LATEST .
 
 docker push $IMAGE
+docker push $IMAGE_LATEST
